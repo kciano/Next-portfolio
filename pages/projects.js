caa478afbaf765931d7ad64dfb81e3d6c6a765e9
kciano <kciano.kc@gmail.com>
2020-09-24 17:34:55 +0200

    Update projects.js

--- pages/projects.js
+++ pages/projects.js
@@ -25,6 +25,10 @@ export default () => {
             <p>This is an application that allows you to search the Itunes database for music, books, podcasts etc</p>
             <p>Click <a href="https://github.com/kciano/Itunes-search">here</a> to view the project on Github</p>
             
+             <br /><br />
+            <h3>Doctor Appointment Management</h3>
+            <p>This is an application that allows users to manage Appointment and Patient's details. On the app, there are two types of users, Admin and Subcriber. The subcriber has a read only account with which they can only view patient and appointment details, whereas admins have full rights where they can create and cancel appointments, and also create, edit and delete patients</p>
+            <p>Click <a href="https://github.com/kciano/docmanage">here</a> to view the project on Github</p>
         </Layout>
     );
 
